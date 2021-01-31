50f6ee6eed3582e4fa09954a76008bfb9ccb35db
Russell Gordon <rgordon@lcs.on.ca>
2021-01-31 15:41:22 -0500

    Initial Commit

---
+++ TitleTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TitleTesting
+//
+//  Created by Russell Gordon on 2021-01-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TitleTesting/TitleTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitleTestingApp.swift
+//  TitleTesting
+//
+//  Created by Russell Gordon on 2021-01-31.
+//
+
+import SwiftUI
+
+@main
+struct TitleTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
